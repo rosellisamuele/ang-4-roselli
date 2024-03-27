b7756797a3e97683475c323ea807ad7ae4af5b91
lai2-24 <lai2-24@itismeucci.com>
2024-03-27 09:42:15 +0000

    Added get

--- list-angular/src/app/employee-table/employee-table.component.ts
+++ list-angular/src/app/employee-table/employee-table.component.ts
@@ -1,12 +1,51 @@
 import { Component } from '@angular/core';
+import { EmployeeService } from '../employee.service';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-employee-table',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './employee-table.component.html',
   styleUrl: './employee-table.component.css'
 })
 export class EmployeeTableComponent {
 
+  data : any = null;
+  
+  constructor(private employeeService : EmployeeService) {}
+
+  page : number = 0;
+  size : number = 20;
+
+  ngOnInit() : void {
+    this.getEmployee();
+  }
+
+  getEmployee() : void {
+    this.employeeService.get(this.page,this.size).subscribe(remoteData => {
+      this.data = remoteData;
+    })
+  }
+
+  changePage(delta : number) : void {
+    if(delta == 1){
+      this.page++;
+      this.getEmployee();
+    }else{
+      this.page--;
+      this.getEmployee();
+    }
+
+  }
+
+  previousPage() : void {
+    this.page = this.page - 1;
+    this.getEmployee();
+  }
+
+  nextPage() : void {
+    this.page = this.page + 1;
+    this.getEmployee();
+  }
 }
--- list-angular/src/app/employee.service.ts
+++ list-angular/src/app/employee.service.ts
@@ -1,9 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
 
-  constructor() { }
+  constructor(private httpClient : HttpClient) { }
+
+  get(page: number, size: number): Observable<any>{
+    return this.httpClient.get("http://localhost:8080/employees");
+  }
 }
